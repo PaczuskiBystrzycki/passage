8cfe4a4c386a2aff692f5f4218919247811eacb4
paczuskibystrzycki <paczuskibystrzycki@gmail.com>
2017-10-04 00:59:40 +0200

    first commit

---
+++ src/main/java/com/paczuskibystrzycki/passage/PassageApplication.java
@@ -0,0 +1,12 @@
+package com.paczuskibystrzycki.passage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassageApplication.class, args);
+	}
+}
