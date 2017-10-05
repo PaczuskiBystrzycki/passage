a6caa2dfee8a9a35f9b25f40fe7bbc21a053db56
paczuskibystrzycki <paczuskibystrzycki@gmail.com>
2017-10-05 23:37:38 +0200

    thymeleaf

---
+++ src/main/java/com/paczuskibystrzycki/passage/login/LoginController.java
@@ -0,0 +1,7 @@
+package com.paczuskibystrzycki.passage.login;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+}
---
+++ src/main/java/com/paczuskibystrzycki/passage/view/ViewController.java
@@ -0,0 +1,14 @@
+package com.paczuskibystrzycki.passage.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String mainView(){
+        return "main";
+    }
+
+}
