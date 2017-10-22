553498a6c0895c3affe018e56c053d0bdf37a954
paczuskibystrzycki <paczuskibystrzycki@gmail.com>
2017-10-22 23:24:36 +0200

    dodanie mapy :)

---
+++ src/main/resources/static/js/controller/datepickerController.js
@@ -0,0 +1,6 @@
+rootModule.controller("datepickerController",function ($scope) {
+
+    $scope.time = "1970-01-01T09:30:40.000Z"; // (formatted: 10:30 AM)
+    $scope.selectedTimeAsNumber = 37840000; // (formatted: 11:30 AM)
+    $scope.sharedDate = "2017-10-22T19:00:00.214Z"; // (formatted: 10/22/17 9:00 PM)
+})
