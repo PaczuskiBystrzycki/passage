4dc1a882915760ddce3c1dad68c04ea6f03f60ae
paczuskibystrzycki <paczuskibystrzycki@gmail.com>
2017-10-22 04:02:48 +0200

    dodanie mapy :)

--- src/main/resources/static/js/controller/mapController.js
+++ src/main/resources/static/js/controller/mapController.js
@@ -1,9 +1,21 @@
-rootModule.controller("mapController",function (NgMap) {
+rootModule.controller("mapController",function (NgMap,$scope) {
 
-    NgMap.getMap().then(function(map) {
-        console.log(map.getCenter());
-        console.log('markers', map.markers);
-        console.log('shapes', map.shapes);
+    $scope.address = 'Ząbki';
+    var d= NgMap.getMap().then(function(map) {
+        gD= map.getCenter();
+
+        console.log('center f',map.getCenter());
+        // //console.log(map.getGeoLocation())
+        // console.log('map geolocataion',map.getGeoLocation)
+        // console.log('markers', map.markers);
+        // console.log('shapes', map.shapes);
     });
 
-})
+
+    $scope.placeChanged = function(dana) {
+        // $scope.place = d.getPlace();
+        // console.log('location', $scope.place.geometry.location);
+        // $scope.map.setCenter(vm.place.geometry.location);
+    }
+
+});
