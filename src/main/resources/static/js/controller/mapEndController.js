d85f595b73f59aee4fe74e4a2bd489cc6112613e
paczuskibystrzycki <paczuskibystrzycki@gmail.com>
2017-10-23 00:38:25 +0200

    dodanie mapy :)

---
+++ src/main/resources/static/js/controller/mapEndController.js
@@ -0,0 +1,23 @@
+rootModule.controller("mapEndController",function (NgMap,$scope) {
+
+    $scope.types = "['establishment']";
+    $scope.address = 'Szaruty';
+    //$scope.mybounds = '{center: {lat: 45, lng: 9}, radius: 5000}';
+    NgMap.getMap().then(function(map) {
+
+    });
+
+
+    $scope.placeChanged = function() {
+        $scope.place = this.getPlace();
+        console.log('location', $scope.place.geometry.location);
+        // console.log('location', $scope.place.geometry);
+        // console.log('location', $scope.place.geometry.d);
+        // console.log('location', $scope.place.geometry.d.d);
+        // console.log('location', $scope.place.geometry.f);
+        // console.log('location', $scope.place.geometry.f.f);
+
+        // $scope.map.setCenter(vm.place.geometry.location);
+    }
+
+});
