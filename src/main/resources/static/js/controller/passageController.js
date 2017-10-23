1d0275b0f8915f809e6f44ac35f26c1b2cfbd278
bstr <bstri@gmail.com>
2017-10-23 21:39:47 +0200

    some changes in GUI + main page

--- src/main/resources/static/js/controller/passageController.js
+++ src/main/resources/static/js/controller/passageController.js
@@ -1,4 +1,17 @@
-rootModule.controller("passageController",function ($scope) {
-    $scope.imie="adam";
-
+rootModule.controller("passageController", function($scope) {
+	$scope.imie = "Lord Mateusz";
 })
+
+
+	$(function(){
+		
+		$('.datepicker').pickadate({
+			selectMonths : true, // Creates a dropdown to control month
+			selectYears : 15, // Creates a dropdown of 15 years to control year,
+			today : 'Today',
+			clear : 'Clear',
+			close : 'Ok',
+			closeOnSelect : false
+		});
+		});
+
--- src/main/resources/static/js/directive/datepickerDirective.js
+++
@@ -1,7 +0,0 @@
-rootModule.directive('datepickerDir',function(){
-    return{
-        restrict: 'E',
-        templateUrl: 'views/fragments/datepicker.htm',
-        controller:'datepickerController',
-    }
-})
